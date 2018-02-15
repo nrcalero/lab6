b2ae720c0d03961e06bdf0c56bd0f892bc424889
Nathanyel Calero <ncalero@ucsd.edu>
2018-02-15 15:21:50 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,30 +1,50 @@
-'use strict';
+(function () {
+  'use strict';
+}());
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
+$(document).ready(function () {
+  initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
-
-	$('#colorBtn').click(randomizeColors);
+  $('.project a').click(addProjectDetails);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+  // Prevents following the link
+  e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+  // Get the div ID, e.g., "project3"
+  var projectID = $(this).closest('.project').attr('id');
 
-	console.log("User clicked on project " + idNumber);
+  // Get rid of 'project' from the front of the id 'project3'
+  var idNumber = projectID.substr('project'.length);
+
+  // Create URL
+  var url = '/project/' + idNumber;
+
+  // Log URL to verify
+  console.log('/project/' + idNumber);
+
+  // GET request
+  $.get(url, addProject);
+}
+
+function addProject(result) {
+  console.log(result);
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result.image + '" class="detailsImage">' +
+    '<p>' + result.title + '</p>' +
+    '<p><small>' + result.date + '</small></p></a>' + result.summary;
+
+  var element = '#project' + result.id + ' .thumbnail .details';
+
+  $(element).html(projectHTML);
 }
